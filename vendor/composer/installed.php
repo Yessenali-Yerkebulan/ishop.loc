e1c537da7fc3d872a75d6b8d545b0587bb2ef5c3
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-07-25 17:19:50 +0600

    configuration of framework

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'edb364825e2cb0f39e7f03090b2483081340bc0f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'edb364825e2cb0f39e7f03090b2483081340bc0f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
