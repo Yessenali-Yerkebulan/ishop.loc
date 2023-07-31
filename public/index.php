2df066c87f79603c4c0a8656ab1e02feefd2d808
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-07-31 16:14:08 +0600

    add Router class and routes

--- public/index.php
+++ public/index.php
@@ -5,8 +5,10 @@
 //}
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS.'/functions.php';
+require_once CONFIG.'/routes.php';
 
 new \wfm\App();
 
-
+debug(\wfm\Router::getRoutes());
 
