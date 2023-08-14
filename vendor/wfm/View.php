22c06af285c0974d93d324d5b266b515f762c718
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-08-14 16:12:32 +0600

    add logs for sql and separate template header, footer etc.

--- vendor/wfm/View.php
+++ vendor/wfm/View.php
@@ -4,6 +4,8 @@
 namespace wfm;
 
 
+use RedBeanPHP\R;
+
 class View
 {
 
@@ -54,4 +56,28 @@ class View
         return $out;
     }
 
+
+    public function getDbLogs(){
+        if(DEBUG){
+            $logs = R::getDatabaseAdapter()
+                ->getDatabase()
+                ->getLogger();
+            $logs = array_merge($logs->grep('SELECT'), $logs->grep('select'),
+            $logs->grep('INSERT'), $logs->grep('UPDATE'), $logs->grep('DELETE'));
+            debug($logs);
+        }
+    }
+
+    public function getPart($file, $data = null){
+        if(is_array($data)){
+            extract($data);
+        }
+
+        $file = APP . "/views/{$file}.php";
+        if(is_file($file)){
+            require $file;
+        }else{
+            echo "File {$file} not found...";
+        }
+    }
 }
